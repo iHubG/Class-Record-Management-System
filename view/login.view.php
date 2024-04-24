d038d6ead688aa301cbaaa80182c8b441b31b209
iHubG <ianmacalinao50@gmail.com>
2024-04-25 01:05:16 +0800

    commit

--- view/login.view.php
+++ view/login.view.php
@@ -8,7 +8,7 @@
     <body>
         <!-- Login -->
         <section id="login">
-            <div class="container-lg">
+            <div class="container">
                 <div class="row justify-content-center my-5">
                     <div class="col-10 shadow" id="login-box">
                         <div class="row">
@@ -16,7 +16,7 @@
                                 <img id="isu-icon" src="./public/img/isu-icon.png" alt="isu icon">
                             </div>
                             <div class="col-6 text-center">
-                                <h2 class="mt-5">Login As</h2>
+                                <h2 id="login-as">Login As</h2>
                                 <div class="d-flex flex-column my-5 gap-5 align-items-center">
                                     <a href="#" class="login-button">Admin</a>
                                     <a href="#" class="login-button">Instructor</a>
