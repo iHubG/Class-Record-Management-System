ca9fb469a8109a453a0d749b6dbaec218238b1c5
iHubG <ianmacalinao50@gmail.com>
2024-04-25 15:48:09 +0800

    commit

--- view/login.view.php
+++ view/login.view.php
@@ -11,13 +11,13 @@
             <div class="container">
                 <div class="row justify-content-center my-5">
                     <div class="col-10 shadow" id="login-box">
-                        <div class="row">
-                            <div class="col-6 d-flex justify-content-center align-items-center" id="login-left">
+                        <div class="row flex-column flex-md-row flex-lg-row" id="login-box2">
+                            <div class="col-12 col-lg-6 d-flex justify-content-center align-items-center" id="login-left">
                                 <img id="isu-icon" src="./public/img/isu-icon.png" alt="isu icon">
                             </div>
-                            <div class="col-6 text-center">
+                            <div class="col-12 col-lg-6 text-center" id="login-right">
                                 <h2 id="login-as">Login As</h2>
-                                <div class="d-flex flex-column my-5 gap-5 align-items-center">
+                                <div class="d-flex flex-column my-5 gap-lg-5 gap-4 align-items-center">
                                     <a href="#" class="login-button">Admin</a>
                                     <a href="#" class="login-button">Instructor</a>
                                     <a href="#" class="login-button">Student</a>
