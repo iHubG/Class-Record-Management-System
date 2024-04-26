7ad3daa1f3db034d184fb904e15b60a38155dbac
iHubG <ianmacalinao50@gmail.com>
2024-04-26 09:31:15 +0800

    commit

--- view/instructor-dash.view.php
+++ view/instructor-dash.view.php
@@ -30,7 +30,10 @@
                                 <span class="placeholder col-6"></span>
                                 <span class="placeholder col-8"></span>
                                 </p>
-                                <a class="btn btn-primary disabled placeholder col-6" aria-disabled="true">Class 1</a>
+                                <div class="d-flex justify-content-between">
+                                    <a class="btn btn-primary disabled placeholder col-6 fs-6" aria-disabled="true">Class 1</a>
+                                    <i class="bi bi-chat fs-4"></i>
+                                </div>
                             </div>
                             </div>
                         </div>
