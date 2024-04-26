48f003c0f9a74711f22f069fbd73365e3de6b2ca
iHubG <ianmacalinao50@gmail.com>
2024-04-27 00:21:19 +0800

    commit

--- view/admin-dash.view.php
+++ view/admin-dash.view.php
@@ -7,23 +7,31 @@
     </head>
     <body>
         <section id="admin-dash">
-            <div class="row m-0 p-0">
+            <div class="row g-0">
                 <!-- Sidebar -->
-                <div class="col-2 d-flex p-3 justify-content-center" id="admin-sidebar">
-                    <h2 class="h3">Admin</h2>
+                <div class="col-2 d-flex p-3 px-5 flex-column" id="admin-sidebar">                   
+                    <h2 class="h4 text-center">Admin</h2>
+                    <nav>
+                        <div class="dash-nav d-flex gap-2 my-4 p-2 rounded">
+                            <i class="bi bi-house"></i>
+                            <h5>Dashboard</h5>
+                        </div>
+                    </nav>
                 </div>
 
                 <!-- Main Content -->
-                <div class="col-10">
+                <div class="col-10 offset-sm-2">
                     <nav class="bg-success-subtle">
-                        <div class="container d-flex justify-content-between align-items-center p-3 ">
+                        <div class="d-flex justify-content-between align-items-center p-3 px-5 ">
                             <h4>Welcome to Admin Dashboard!</h4>
                             <i class="bi bi-person-circle fs-3"></i>
                         </div>  
                     </nav>
+                    <div class="some-text">
+                        
+                    </div>
                 </div>
             </div>
-           
         </section>
     </body>
 </html>
