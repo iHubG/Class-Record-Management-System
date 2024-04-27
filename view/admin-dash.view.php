d9f75db0cbb533b3130b45d8f30c3143743299ed
iHubG <ianmacalinao50@gmail.com>
2024-04-28 00:09:31 +0800

    commit

--- view/admin-dash.view.php
+++ view/admin-dash.view.php
@@ -9,7 +9,7 @@
         <section id="admin-dash">
             <div class="row g-0">
                 <!-- Sidebar -->
-                <div class="col-2 d-flex p-3 px-2 flex-column align-items-" id="admin-sidebar">                   
+                <div class="col-0 d-none d-sm-block d-sm-none d-md-block d-md-none d-lg-block col-lg-2 d-flex p-3 px-2 flex-column" id="admin-sidebar">                   
                     <h2 class="h4 text-center">Admin</h2>
                     <nav>
                         <div class="dash-nav d-flex gap-2 my-4 p-2 rounded">
@@ -23,10 +23,22 @@
                     </nav>
                 </div>
 
+                <!-- OffCanvas -->
+                <div class="offcanvas offcanvas-start w-50" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Offcanvas with body scrolling</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <p>Try scrolling the rest of the page to see this option in action.</p>
+                    </div>
+                </div>
+
                 <!-- Main Content -->
-                <div class="col-10 offset-sm-2" id="admin-main-content">
+                <div class="col-12 col-lg-10 offset-lg-2" id="admin-main-content">
                     <nav class="bg-success-subtle">
                         <div class="d-flex justify-content-between align-items-center p-3 px-3 ">
+                            <i class="bi bi-list d-lg-none d-xl-block d-xl-none d-xxl-block d-xxl-none fs-3" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling" id="burger-menu"></i>
                             <h4>Welcome to Admin Dashboard!</h4>
                             <i class="bi bi-person-circle fs-3"></i>
                         </div>  
